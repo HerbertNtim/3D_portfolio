60d3c08eea8070ff523bfeb493477184dde64955
Herbert <herbertntim15@gmail.com>
2025-04-19 08:21:23 +0000

    testing auto merge

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ const App = () => {
         <h1 className="text-3xl">Welcome to 3D Portfolio!</h1>
         <p>This is a work in progress</p>
         <p>Testing auto merge</p>
+        <p>Testing auto merge</p>
       </div>
     </main>
   )
