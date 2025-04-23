2c4fdc9bd2a7836db0cf541301df495730fd751d
HerbertNtim <herbertntim15@gmail.com>
2025-04-23 11:23:39 +0000

    dev: 3d model position well

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -1,17 +1,33 @@
-import { Canvas } from '@react-three/fiber'
-import React from 'react'
-import { useMediaQuery } from 'react-responsive'
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { useMediaQuery } from "react-responsive";
+
+import { Room } from "./Room";
+import HeroLights from "./HeroLights";
 
 const HeroExperience = () => {
-  const isTablet = useMediaQuery({ query: '(max-width: 1024px)' });
-  const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
+  const isTablet = useMediaQuery({ query: "(max-width: 1024px)" });
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
 
   return (
-    <Canvas camera={{position: [ 0, 0, 15], fov: 45}}>
-      <ambientLight intensity={0.2} color="#1a1a40" />
-      <directionalLight position={[5, 5, 5]} intensity={1}  />
-    </Canvas>
-  )
-}
+    <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
+      
+      <HeroLights />
 
-export default HeroExperience
+      <OrbitControls
+        enableZoom={!isTablet}
+        enablePan={false}
+        maxDistance={20}
+        minDistance={5}
+        minPolarAngle={Math.PI / 5}
+        maxPolarAngle={Math.PI / 2}
+      />
+
+      <group scale={isMobile ? 0.7 : 1} position={[0, -3.5, 0]} rotation={[0, -Math.PI / 4, 0]}>
+        <Room />
+      </group>
+    </Canvas>
+  );
+};
+
+export default HeroExperience;
