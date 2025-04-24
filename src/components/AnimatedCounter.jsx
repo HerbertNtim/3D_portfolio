b46f63787c8836c25a2fd0161ac2dabeb64cf2d7
HerbertNtim <herbertntim15@gmail.com>
2025-04-24 13:42:31 +0000

    feat: implemented count up

--- src/components/AnimatedCounter.jsx
+++ src/components/AnimatedCounter.jsx
@@ -1,4 +1,5 @@
 import { counterItems } from "../constants";
+import CountUp from "react-countup";
 
 const AnimatedCounter = () => {
   return (
@@ -13,7 +14,7 @@ const AnimatedCounter = () => {
             className="bg-zinc-900 rounded-lg p-8 flex flex-col justify-center"
           >
             <div className="counter-number text-white-50 text-5xl font-bold mb-2">
-              0 {item.suffix}
+              <CountUp suffix={item.suffix} end={item.value} />
             </div>
             <div className="text-white-50 text-lg">{item.label}</div>
           </div>
