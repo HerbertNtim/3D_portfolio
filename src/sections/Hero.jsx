bca025a55d611380db1b7384da9c41ea0ef674f4
HerbertNtim <herbertntim15@gmail.com>
2025-04-24 10:34:18 +0000

    dev: implemented animation to hero text

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,7 +3,26 @@ import { words } from "../constants";
 import Button from "../components/Button";
 import HeroExperience from "../components/HeroModels/HeroExperience";
 
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+
 const Hero = () => {
+  useGSAP(() => {
+    gsap.fromTo(".hero-text h1", 
+      {
+        y: 50,
+        opacity: 0, 
+      }, {
+        y: 0,
+        opacity: 1,
+        duration: 1,
+        stagger: 0.2,
+        ease: "power2.inout",
+      }
+    );
+  });
+
   return (
     <section id="hero" className="relative overflow-hidden">
       <div className="absolute top-0 left-0 z-10">
