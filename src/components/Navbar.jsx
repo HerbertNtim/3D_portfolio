6afbce11e0e231d16f7e442b9fd3f42060ed51c1
HerbertNtim <herbertntim15@gmail.com>
2025-04-25 14:03:58 +0000

    [feat]: added navbar scroll effect

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
     }
 
     window.addEventListener('scroll', handleScroll)
-    window.removeEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
   }, [])
 
   return (
